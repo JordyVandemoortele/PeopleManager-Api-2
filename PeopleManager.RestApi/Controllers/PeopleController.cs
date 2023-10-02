791a0115a9b80ec468c8445995bb71232272f72f
JordyVandemoortele <125571637+JordyVandemoortele@users.noreply.github.com>
2023-10-02 16:12:25 +0200

    Initial commit

---
+++ PeopleManager.RestApi/Controllers/PeopleController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PeopleManager.Model;
+using PeopleManager.Services;
+
+namespace PeopleManager.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private readonly PersonService _personService;
+
+        public PeopleController(PersonService personService)
+        {
+            _personService = personService;
+        }
+
+        [HttpGet]
+        public IActionResult Find()
+        {
+            var people = _personService.Find();
+            return Ok(people);
+        }
+
+        [HttpGet("{id:int}", Name = "GetPersonRoute")]
+        public IActionResult Get(int id)
+        {
+            var people = _personService.Get(id);
+            return Ok(people);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]Person model)
+        {
+            var person = _personService.Create(model);
+            if (person is null)
+            {
+                return NotFound();
+            }
+            return CreatedAtRoute("GetPersonRoute", new {id = person.Id}, person);
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult Edit(int id, Person model)
+        {
+            var person = _personService.Update(id, model);
+            if (person is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(person);
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            _personService.Delete(id);
+
+            return Ok();
+        }
+    }
+}
